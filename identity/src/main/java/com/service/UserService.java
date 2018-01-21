a0c3790510ac26f347e2b1348b5bd18a7cccf571
dimaNCintern <workday2006@gmail.com>
2018-01-21 22:57:27 +0300

    init

--- identity/src/main/java/com/service/UserService.java
+++ identity/src/main/java/com/service/UserService.java
@@ -3,10 +3,11 @@ package com.service;
 import com.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by dima on 21.01.18.
  */
 public interface UserService {
-    List<User> findAll();
+    Optional<List<User>> findAll();
 }
