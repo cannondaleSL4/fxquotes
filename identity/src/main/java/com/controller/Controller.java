e8d8ef8960fbbce5d010286c0838c9aff510d936
dimaNCintern <workday2006@gmail.com>
2018-01-22 04:56:21 +0300

    init

---
+++ identity/src/main/java/com/controller/Controller.java
@@ -0,0 +1,10 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dima on 22.01.18.
+ */
+@RestController
+public class Controller {
+}
