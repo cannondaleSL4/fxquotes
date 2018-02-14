c28ddd89c284da85a7d1a397a6a6d3e423d37a83
dimaNCintern <workday2006@gmail.com>
2018-02-14 23:48:06 +0300

    some changes

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'spring boot and angular 4 secure authentication';
