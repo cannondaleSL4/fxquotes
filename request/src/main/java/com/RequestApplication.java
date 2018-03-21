8c81bd725d7c8f19d8c98d02c2e921cc0dbef574
dimaNCintern <workday2006@gmail.com>
2018-03-21 23:40:51 +0300

    some changes

---
+++ request/src/main/java/com/RequestApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dima on 21.01.18.
+ */
+@SpringBootApplication
+public class RequestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RequestApplication.class, args) ;
+    }
+}
