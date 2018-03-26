bccf6a409a17f122797508c2e20d8a4b983c7ac1
Dima <workday2006@yandex.ru>
2018-03-26 10:56:27 +0300

    some change

--- startall.sh
+++ startall.sh
@@ -8,3 +8,16 @@ mvn package
 cd ../gateway/
 mvn spring-boot:run
 
+#make and run identity module
+cd ../identity/
+mvn spring-boot:run
+
+#make and run request module
+cd ../request
+mvn spring-boot:run
+
+#run angular
+cd ../angular/
+fuser -k 4200/tcp
+ng serve --open
+
