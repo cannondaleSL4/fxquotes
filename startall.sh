6ae222ebc389373b8a96194a89e74c02b9860a3e
dimaNCintern <workday2006@gmail.com>
2018-03-28 23:07:20 +0300

    Squashed commit of the following:
    commit 9dc9240fd1cdbee8eae7eea275486f885dbeb439
    Author: dimaNCintern <workday2006@gmail.com>
    Date:   Tue Mar 27 23:54:07 2018 +0300
    
        change pom files
    
    commit 95b469b409d44f131698efb734bf47275f384f6a
    Author: dimaNCintern <workday2006@gmail.com>
    Date:   Tue Mar 27 23:40:26 2018 +0300
    
        change pom files
    
    commit cb2582f0566a7331508a1da06f5b1cb11e4875a3
    Author: dimaNCintern <workday2006@gmail.com>
    Date:   Tue Mar 27 23:31:14 2018 +0300
    
        change pom files
    
    commit c7f667ad0ace24b3725260170b80afbf7eed1a66
    Author: dimaNCintern <workday2006@gmail.com>
    Date:   Tue Mar 27 22:57:05 2018 +0300
    
        change pom files
    
    commit 9d48fdbd732d10aae941089823cca5cae5e291cc
    Author: Dima <workday2006@yandex.ru>
    Date:   Tue Mar 27 19:00:30 2018 +0300
    
        refactoring pom
    
    commit c9972852669ffeb8c21ca4868f05e11166de0e34
    Author: Dima <workday2006@yandex.ru>
    Date:   Tue Mar 27 18:43:17 2018 +0300
    
        refactoring pom
    
    commit 316a96b120ba4063a1c26631ff47d66cdabf51ea
    Author: Dima <workday2006@yandex.ru>
    Date:   Tue Mar 27 18:00:30 2018 +0300
    
        refactoring pom
    
    commit ac1748b56a9f22422dc046170f64cf0f7b4f8205
    Author: Dima <workday2006@yandex.ru>
    Date:   Tue Mar 27 10:34:21 2018 +0300
    
        refactoring pom
    
    commit dcc75a4d29959bc04d335e82bac768af04f44a96
    Author: Dima <workday2006@yandex.ru>
    Date:   Tue Mar 27 10:27:46 2018 +0300
    
        refactoring pom
    
    commit 1690114c59b9fec1a1023f1f99f376500dd66bd7
    Author: Dima <workday2006@yandex.ru>
    Date:   Tue Mar 27 09:43:57 2018 +0300
    
        refactoring pom
    
    commit 99623cc5ea7fc4531c53def05c9629a1f1342d41
    Author: Dima <workday2006@yandex.ru>
    Date:   Tue Mar 27 09:41:34 2018 +0300
    
        add to stop script
    
    commit acab2fd4cabb9fc30aecb608ecfb5ba3b461282b
    Author: dimaNCintern <workday2006@gmail.com>
    Date:   Tue Mar 27 00:13:55 2018 +0300
    
        change pom files
    
    commit 7656b9a9038bb94341c704ddefdd53cd63694241
    Author: Dima <workday2006@yandex.ru>
    Date:   Mon Mar 26 21:28:40 2018 +0300
    
        after global crush
    
    commit ed4067bbfc2212b0f0f9f4a8419afb6d2c4f2b94
    Author: Dima <workday2006@yandex.ru>
    Date:   Mon Mar 26 11:05:58 2018 +0300
    
        some change
    
    commit bccf6a409a17f122797508c2e20d8a4b983c7ac1
    Author: Dima <workday2006@yandex.ru>
    Date:   Mon Mar 26 10:56:27 2018 +0300
    
        some change

--- startall.sh
+++ startall.sh
@@ -4,36 +4,23 @@ chmod +x ./stop.sh
 ./stop.sh
 
 #package for entity module
-mvn -f ./entity/ package
+mvn -f ./entity/ clean install
 #make gateway module
-mvn -f ./gateway/ package
+mvn -f ./gateway/ clean install
 #make identity module
-mvn -f ./identity/ package
+mvn -f ./identity/ clean install
 #make and run request module
-mvn -f ./request/ package
+mvn -f ./request/ clean install
 
 chmod +x ./gateway/target/gateway-1.0-SNAPSHOT.jar
 chmod +x ./identity/target/identity-1.0-SNAPSHOT.jar
 chmod +x ./request/target/request-1.0-SNAPSHOT.jar
 
-java -jar ./gateway/target/*.jar &
-java -jar ./identity/target/*.jar &
-java -jar ./request/target/*.jar &
+java -jar ./gateway/target/*.jar \
+   && java -jar ./identity/target/*.jar \
+   && java -jar ./request/target/*.jar
 
 #run angular
-cd ./angular/
-ng serve --open
-
-#make and run identity module
-cd ../identity/
-mvn spring-boot:run
-
-#make and run request module
-cd ../request
-mvn spring-boot:run
-
-#run angular
-cd ../angular/
-fuser -k 4200/tcp
-ng serve --open
+#cd ./angular/
+#ng serve --open
 
