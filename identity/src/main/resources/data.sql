ceef5bb32ec89d95bfa2db844262bb10d5d532cd
dmitriy <workday2006@gmail.com>
2018-05-15 21:16:14 +0300

    init

--- identity/src/main/resources/data.sql
+++ identity/src/main/resources/data.sql
@@ -8,14 +8,3 @@ INSERT INTO USERAPP (email,password,is_active)  VALUES ('test@test.com','test',T
 INSERT INTO USER_ROLE (id_user,id_role) VALUES ((SELECT id_user FROM USERAPP WHERE email = 'dima@dima.com'),(SELECT id_role FROM USERROLE WHERE rolename = 'ADMIN'));
 INSERT INTO USER_ROLE (id_user,id_role) VALUES ((SELECT id_user FROM USERAPP WHERE email = 'dima@dima.com'),(SELECT id_role FROM USERROLE WHERE rolename = 'USER'));
 INSERT INTO USER_ROLE (id_user,id_role) VALUES ((SELECT id_user FROM USERAPP WHERE email = 'test@test.com'),(SELECT id_role FROM USERROLE WHERE rolename = 'USER'));
-
--- INSERT INTO USER_ROLE (id_user_role,id_role,id_user) VALUES(1,1,1);
--- INSERT INTO USER_ROLE (id_user_role,id_role,id_user) VALUES(2,2,1);
--- INSERT INTO USER_ROLE (id_user_role,id_role,id_user) VALUES(3,2,2);
-
-
--- CREATE TABLE IF NOT EXISTS USER_ROLE(
---   id_user_role integer AUTO_INCREMENT,
---   id_role integer, FOREIGN KEY (id_role) REFERENCES USERROLE (id_role),
---   id_user integer, FOREIGN KEY (id_user) REFERENCES USERAPP (id_user)
--- );
